b46680072cdd8e3716e33df5487637785d4ee4cf
Katia Fredes <katia.fredes.a@gmail.com>
2024-04-30 16:19:12 -0400

    add mtree class

--- Tarea1/src/MTree.cpp
+++ Tarea1/src/MTree.cpp
@@ -1,48 +1,57 @@
+#include <iostream>
+#include "MTree/Node/Entry.h"
+
 class MTree {
 public:
-    MTree() {
-        root = new MTreeNode();
+    Node &root;
+    double B;
+    double b;
+
+    MTree(double B, double b, Node &root) : root(root) {
+        this->B = B;
+        this->b = b;
+        // root = new Node();
     }
 
-    void insert(vector<int> key) {
-        MTreeNode* cur = root;
-        for (int i = 0; i < key.size(); i++) {
-            if (cur->children.find(key[i]) == cur->children.end()) {
-                cur->children[key[i]] = new MTreeNode();
-            }
-            cur = cur->children[key[i]];
-        }
-    }
+    // void insert(vector<int> key) {
+    //     MTreeNode* cur = root;
+    //     for (int i = 0; i < key.size(); i++) {
+    //         if (cur->children.find(key[i]) == cur->children.end()) {
+    //             cur->children[key[i]] = new MTreeNode();
+    //         }
+    //         cur = cur->children[key[i]];
+    //     }
+    // }
 
-    bool search(vector<int> key) {
-        MTreeNode* cur = root;
-        for (int i = 0; i < key.size(); i++) {
-            if (cur->children.find(key[i]) == cur->children.end()) {
-                return false;
-            }
-            cur = cur->children[key[i]];
-        }
-        return true;
-    }
+    // bool search(vector<int> key) {
+    //     MTreeNode* cur = root;
+    //     for (int i = 0; i < key.size(); i++) {
+    //         if (cur->children.find(key[i]) == cur->children.end()) {
+    //             return false;
+    //         }
+    //         cur = cur->children[key[i]];
+    //     }
+    //     return true;
+    // }
 
-    void remove(vector<int> key) {
-        MTreeNode* cur = root;
-        vector<MTreeNode*> path;
-        for (int i = 0; i < key.size(); i++) {
-            if (cur->children.find(key[i]) == cur->children.end()) {
-                return;
-            }
-            path.push_back(cur);
-            cur = cur->children[key[i]];
-        }
-        for (int i = key.size() - 1; i >= 0; i--) {
-            if (cur->children.size() > 0) {
-                break;
-            }
-            delete cur;
-            cur = path.back();
-            path.pop_back();
-            cur->children.erase(key[i]);
-        }
-    }
-}
+    // void remove(vector<int> key) {
+    //     MTreeNode* cur = root;
+    //     vector<MTreeNode*> path;
+    //     for (int i = 0; i < key.size(); i++) {
+    //         if (cur->children.find(key[i]) == cur->children.end()) {
+    //             return;
+    //         }
+    //         path.push_back(cur);
+    //         cur = cur->children[key[i]];
+    //     }
+    //     for (int i = key.size() - 1; i >= 0; i--) {
+    //         if (cur->children.size() > 0) {
+    //             break;
+    //         }
+    //         delete cur;
+    //         cur = path.back();
+    //         path.pop_back();
+    //         cur->children.erase(key[i]);
+    //     }
+    // }
+};
