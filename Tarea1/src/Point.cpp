038cfabfc4e952ac412900c46bd0b3b2d5030a05
Ephy <vicente.ardiles@ug.uchile.cl>
2024-05-01 01:36:39 -0400

    update getters name

--- Tarea1/src/Point.cpp
+++ Tarea1/src/Point.cpp
@@ -6,16 +6,16 @@ Point::Point() : x(0), y(0) {}
 Point::Point(double x, double y) : x(x), y(y) {}
 
 // Getters
-double Point::get_X() {
-    return this->x;
+double Point::get_x() {
+    return x;
 }
 
-double Point::get_Y() {
-    return this->y;
+double Point::get_y() {
+    return y;
 }
 
 
 double dist(Point p, Point q)
 {   
-    return sqrt(pow(p.get_X() - q.get_X(), 2) + pow(p.get_Y() - q.get_Y(), 2));
+    return sqrt(pow(p.get_x() - q.get_x(), 2) + pow(p.get_y() - q.get_y(), 2));
 }
