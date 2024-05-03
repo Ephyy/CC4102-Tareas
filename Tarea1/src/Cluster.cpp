d4be6b1ee5d5fa8b9aa73f3e8889d41ff31a24bc
Katia Fredes <katia.fredes.a@gmail.com>
2024-05-02 23:32:39 -0400

    add cluster related functions

--- Tarea1/src/Cluster.cpp
+++ Tarea1/src/Cluster.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 #include "Point.h"
 
 using namespace std;
 
 class Cluster {
     public:
+        // Ver si es mejor usar otra estructura de datos
         vector<Point> points;
         Point primary_medoid;
         double radius;
-        double max_size;
+        // B 
+        double max_size; 
+        // b
         double min_size;
 
         Cluster() {
@@ -17,5 +21,22 @@ class Cluster {
             max_size = 0;
             min_size = 0;
         }
-        
+
+        // Obtains the primary medoid from the points in the cluster:
+        // the least medoid of the cluster in this order.
+        // 
+        // A cluster medoid is any point m in the cluster for which there exists no other
+        // point p in the cluster that, if nominated as a medoid, would give the cluster
+        // a radius less than that given by m.
+        Point set_primary_medoid();
+
+        // The distance between two clusters is that between their primary medoids.
+        double distance(Cluster cluster);
+
+        // Given a cluster c, a nearest neighbour of c in a set of clusters C is a cluster c'
+        // such that there is no other cluster in C whose distance to c is less than
+        // that between c and c'. (c may have multiple nearest neighbours in C.)
+        Cluster nearest_neighbour(vector<Cluster> clusters);
 };
+
+pair<Cluster, Cluster> closest_pair(vector<Cluster> clusters);
