523015215bb5a9a475134b3ec50ba2ed5f4d8efa
Katia Fredes <katia.fredes.a@gmail.com>
2024-05-04 22:09:17 -0400

    add bulkLoad implementation

--- Tarea1/src/MTreeSS.cpp
+++ Tarea1/src/MTreeSS.cpp
@@ -43,7 +43,6 @@ class MTreeBySS {
                 r = max(r, dist(g, *p));
             }
 
-            // Memory is dynamically allocated for the Node object using new
             shared_ptr<Node> a = make_shared<Node>(C);
             return Entry(g, r, a);
         }
@@ -71,17 +70,66 @@ class MTreeBySS {
                 R = max(R, dist(G, e.get_p()));
             }
             
-            // Memory is dynamically allocated for the Node object using new
             shared_ptr<Node> A = make_shared<Node>(C);
             return Entry(G, R, A);
         }
 
         // Function BulkLoad(
-        // Cin: a set of points in a metric space,
-        // CMAX: maximum number of node entries that will fit into a disk page
+        //      Cin: a set of points in a metric space,
+        //      CMAX: maximum number of node entries that will fit into a disk page
         // )
-        // Returns disk address of root page of constructed M-tree
-        Node bulkLoad(vector<Point> &c_in);
+        // Returns disk address of root page of constructed M-tree.
+        shared_ptr<Node> bulkLoad(double max_size, vector<shared_ptr<Point>> points) {
+            if (points.size() <= max_size) {
+                Cluster c_in(max_size);
+                for (shared_ptr<Point> p : points) {
+                    c_in.insert(p);
+                }
+                Entry entry = outputLeafPage(c_in);
+                return entry.get_a();
+            }
 
+            vector<Cluster> clusters_output = cluster_fun(max_size, points);
+            // Let C = {}, entries allocated
+            vector<Entry> entries;
+            for_each(clusters_output.begin(), clusters_output.end(), [&entries, this] (Cluster c) {
+                // Add OutputLeafPage(c) to C
+                entries.push_back(outputLeafPage(c));
+            });
+            while (entries.size() > max_size) {
+                // Let Cin = {m|(m, r,a) ∈ C}
+                vector<shared_ptr<Point>> new_points;
+                for (Entry e : entries) {
+                    new_points.push_back(make_shared<Point>(e.get_p())); // make_shared llama a uno nuevo
+                }
+                // Let Cout = Cluster(Cin, CMAX)
+                vector<Cluster> clusters_output = cluster_fun(max_size, new_points);
+                // Let Cmra = {}
+                vector<Entry> entries_mra;
+                for_each(clusters_output.begin(), clusters_output.end(), [&entries_mra, &entries, this] (Cluster cluster) {
+                    // Let s = {(m, r, a)|(m, r, a) ∈ C ∧ m ∈ c} 
+                    // todas las entradas que están en entries (C) y que su punto está en el cluster c
+                    for_each(entries.begin(), entries.end(), [&entries_mra, &cluster] (Entry entry) {
+                        auto it = find_if(cluster.points.begin(), cluster.points.end(), [&entry] (shared_ptr<Point> point) {
+                            return entry.get_p() == *point;
+                        });
+                        if (it != cluster.points.end()) {
+                            entries_mra.push_back(entry);
+                        }
+                    });
+                });
+                
+                // vector<Entry> new_entries;
+                // new_entries.push_back(outputInternalPage(entries_mra));
+                // Let C = {} ;
+                entries.clear();
+                // For each s ∈ Cmra
+                //     Add OutputInternalPage(s) to C ; --> no entiendo bien a que se refiere???
+                entries.push_back(outputInternalPage(entries_mra));
+            }
+            // Let (m, r, a) = OutputInternalPage(C)
+            Entry entry = outputInternalPage(entries);
+            return entry.get_a();
+        }
         
 };
