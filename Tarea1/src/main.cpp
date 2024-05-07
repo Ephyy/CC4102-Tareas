c5f5cc1f52a50fad197daec60cce158546d0fe6d
Katia Fredes <katia.fredes.a@gmail.com>
2024-05-06 23:39:25 -0400

    radom ponts from 0

--- Tarea1/src/main.cpp
+++ Tarea1/src/main.cpp
@@ -17,20 +17,13 @@ int main() {
     std::cout << e1.get_a()->get_B() << std::endl;
 
     // MTREE BY SS TESTING
-    vector<Point> points = generateRandomPoints(10, 10);
-    cout << "Points: " << endl;
-
-    for (Point p : points) {
-        cout << p << endl;
-    } 
-    
+    vector<Point> points = generateRandomPoints(100, 1);
     vector<shared_ptr<Point>> shared_points;
     for (Point p : points) {
         shared_points.push_back(make_shared<Point>(p));
     }
     points.clear();
-
-    MTreeBySS mtree = MTreeBySS(4);
+    MTreeBySS mtree = MTreeBySS(10);
 
     cout << "Setting node..." << endl;
     mtree.set_node(shared_points);
@@ -49,10 +42,10 @@ int main() {
     for (Entry e : mtree.node->get_entries()[1].get_a()->get_entries()) {
         cout << e.get_p() << endl;
     }
-    cout << "Entries third son Node: " << endl;
-    for (Entry e : mtree.node->get_entries()[2].get_a()->get_entries()) {
-        cout << e.get_p() << endl;
-    }
+    // cout << "Entries third son Node: " << endl;
+    // for (Entry e : mtree.node->get_entries()[2].get_a()->get_entries()) {
+    //     cout << e.get_p() << endl;
+    // }
 
     return 0;
 }
