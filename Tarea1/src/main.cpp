4a97ed195de118e38c176d9b8209f3701632bcb6
Ephy <vicente.ardiles@ug.uchile.cl>
2024-05-11 15:32:25 -0400

    update main.cpp

--- Tarea1/src/main.cpp
+++ Tarea1/src/main.cpp
@@ -4,17 +4,17 @@
 #include "Node.h"
 #include "Entry.h"
 
-int main()
-{
-    std::cout << "TESTING" << std::endl;
+// int main()
+// {
+//     std::cout << "TESTING" << std::endl;
 
 
-    Point p = Point(1, 2);
-    auto n1 = std::make_shared<Node>(2);
-    Entry e1 = Entry(p, 3, n1);
+//     Point p = Point(1, 2);
+//     auto n1 = std::make_shared<Node>(2);
+//     Entry e1 = Entry(p, 3, n1);
 
-    std::cout << e1.get_a()->get_B() << std::endl;
+//     std::cout << e1.get_a()->get_B() << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
