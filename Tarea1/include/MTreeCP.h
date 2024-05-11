b30df1ea438a64c5a1cafa8ab3e78d48e636bef9
Ephy <vicente.ardiles@ug.uchile.cl>
2024-05-11 15:32:07 -0400

    add MTree.h file

---
+++ Tarea1/include/MTreeCP.h
@@ -0,0 +1,7 @@
+// std::ostream& operator<<(std::ostream& os, const std::vector<int>& v);
+// std::ostream& operator<<(std::ostream& os, const std::vector<vector<int>>& vv);
+// std::ostream& operator<<(std::ostream& os, const std::vector<Point>& v);
+// std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<Point>>& vv);
+// std::ostream& operator<<(std::ostream& os, const std::map<int, vector<int>>& m);
+// std::ostream& operator<<(std::ostream& os, const std::map<int, Node>& n);
+// std::ostream& operator<<(std::ostream& os, const std::map<int, std::shared_ptr<Node>>& n);
