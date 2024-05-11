4343dd421242bd6345b33131ec9fd52efcc371e9
Ephy <vicente.ardiles@ug.uchile.cl>
2024-05-11 15:31:44 -0400

    cp tests

--- Tarea1/tests/cp_tests.cc
+++ Tarea1/tests/cp_tests.cc
@@ -44,4 +44,25 @@ TEST(NodeTest, Height_test) {
     EXPECT_EQ(n.height(), 3);
 }
 
+TEST(NodeTest, Validate_test) {
+    int B = 10;
+    Node n = Node(B);
+    vector<Point> points = {Point({1, 1}), Point({2, 2}), Point({3, 3})};
+
+    Entry e = Entry(Point({1, 1}), 0, nullptr);
+    n.insert(e);
+
+    vector<Point> points2 = points;
+    EXPECT_EQ(n.validate(points2), false);
+
+    shared_ptr<Node> n2 = make_shared<Node>(B);
+    Entry e_leaf = Entry(Point({3, 3}), 0, nullptr);
+    n2->insert(e_leaf);
+
+    Entry e2 = Entry(Point({2, 2}), 0, n2);
+    n.insert(e2);
+
+    vector<Point> points3 = points;
+    EXPECT_EQ(n.validate(points3), true);
+}
 
