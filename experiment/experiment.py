f14ff448a051b4820a1c0a27f9e61c99527ef8a0
belen <isajannis@gmail.com>
2024-05-12 01:35:23 -0400

    added normal distribution around given data

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -5,8 +5,14 @@ import math
 # El eje X son los accesos
 # El eje Y el conteo de esos accesos
 
-x = np.array([1, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 10])
+x = np.array([1, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 8, 8, 8, 9, 10])
 y = np.array([0, 1, 4, 7, 10, 17, 13, 4, 0])
+
+# Experimentación en una experimentación supongo
+# Para hacer una distribución normal basada en estos valores x
+mediaX = np.mean(x)
+desviacionEstandarX = np.std(x)
+datosNormal = np.random.normal(mediaX, desviacionEstandarX, 1000)
 
 # Normal con media 5, desviación estándar y 10000 números generados para esta muestra
 datos_normal = np.random.normal(5, 1, 10000)
@@ -20,7 +26,7 @@ n = 10
 # density: normalizar la altura de cada contenedor (influirá si ambos trabajan con 100 datos?)
 # alpha: transparencia/opacidad de las barras
 plt.hist(x, bins=25, density=True, alpha=0.5, color='r', label='Método CP')
-plt.hist(datos_normal, bins=10, density=True, alpha=0.5, color='b', label='Método SS')
+plt.hist(datosNormal, bins=25, density=True, alpha=0.5, color='b', label='Método SS')
 
 # Labels
 plt.xlabel('Cantidad de accesos')
@@ -29,7 +35,7 @@ plt.title('Distribución de accesos para 2^' + str(n) + ' puntos')
 plt.legend()
 
 # Para dibujar la curva
-mu, sigma = np.mean(datos_normal), np.std(datos_normal)
+mu, sigma = np.mean(datosNormal), np.std(datosNormal)
 x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
 plt.plot(x, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x - mu) / sigma) ** 2), color='r')
 
