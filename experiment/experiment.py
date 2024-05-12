cb18520325b22f9b442ae83b02a78b2fc9bc84ff
belen <isajannis@gmail.com>
2024-05-12 19:22:40 -0400

    new plot code

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,11 +1,11 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 # Exponente que indica la cantidad de puntos del set P
 # Es para el label más que nada
 # Y para el nombre del archivo ahora
-n = 21
+n = 14
 
 # Para el método CP
 fileNameCP = 'experimento_CP_' + str(n) + '.tsv'
@@ -13,39 +13,49 @@ dfCP = pd.read_csv(fileNameCP, sep = '\t')
 # display(dfCP)
 
 # Para el método SS
-# fileNameSS = 'experimento_SS_' + str(n) + '.tsv'
-# dfSS = pd.read_csv(fileNameSS, sep = '\t')
+fileNameSS = 'wsl_experimento_SS_' + str(n) + '.tsv'
+dfSS = pd.read_csv(fileNameSS, sep = '\t')
 
 # El eje X son los accesos
 # El eje Y el conteo de esos accesos
-
 iosDataCP = dfCP['ios'].values
-# iosDataSS = dfSS['ios'].values
+iosDataSS = dfSS['ios'].values
 
-# Experimentación en una experimentación supongo
-# Para hacer una distribución normal basada en estos valores x
-# mediaX = np.mean(dataCP)
-# desviacionEstandarX = np.std(dataCP)
-# datosNormal = np.random.normal(mediaX, desviacionEstandarX, 1000)
+# Datos para CP
+promedioCP = iosDataCP.mean()
+stdCP = iosDataCP.std()
+intervaloConfianzaCP = 1.96 * (promedioCP / np.sqrt(len(iosDataCP)))
+
+# Datos para SS
+promedioSS = iosDataSS.mean()
+stdSS = iosDataSS.std()
+intervaloConfianzaSS = 1.96 * (promedioSS / np.sqrt(len(iosDataSS)))
+
+# Printeo de datos
+print(" ")
+print("Datos del método CP")
+print("Promedio CP: " + str(promedioCP))
+print("Desviación estándar CP: " + str(stdCP))
+print("Intervalo de Confianza al 95%: " + str(intervaloConfianzaCP))
+print(" ")
+print("Datos del método SS")
+print("Promedio SS: " + str(promedioSS))
+print("Desviación estándar SS: " + str(stdSS))
+print("Intervalo de Confianza al 95%: " + str(intervaloConfianzaSS))
+print(" ")
 
 # Se arman los histogramas a partir de los datos que supuestamente ya tenemos
 # bins: cantidad de "contenedores" para dividir los datos
 # density: normalizar la altura de cada contenedor (influirá si ambos trabajan con 100 datos?)
 # alpha: transparencia/opacidad de las barras
-plt.hist(iosDataCP, bins=25, density=False, alpha=0.5, color='r', label='Método CP')
-# plt.hist(dataSS, bins=25, density=False, alpha=0.5, color='b', label='Método SS')
-# plt.hist(datosNormal, bins=25, density=True, alpha=0.5, color='g', label='Distribución normal')
+plt.hist(iosDataCP, bins=10, density=False, alpha=0.5, color='r', label='Método CP')
+plt.hist(iosDataSS, bins=10, density=False, alpha=0.5, color='b', label='Método SS')
 
 # Labels
 plt.xlabel('Cantidad de accesos')
 plt.ylabel('Frecuencia')
 plt.title('Distribución de accesos para 2^' + str(n) + ' puntos')
 plt.legend()
-
-# Para dibujar la curva
-# mu, sigma = np.mean(datosNormal), np.std(datosNormal)
-# x_dn = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-# plt.plot(x_dn, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x_dn - mu) / sigma) ** 2), color='r')
 
 # Mostrar el histograma
 plt.show()
