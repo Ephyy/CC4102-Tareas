010df359f15b0593aa60088b56b0c405cbd31b1f
Katia Fredes <katia.fredes.a@gmail.com>
2024-05-12 02:21:22 -0400

    add count of ios in disk is lookup

--- Tarea1/src/RangeQuery.cpp
+++ Tarea1/src/RangeQuery.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 RangeQuery::RangeQuery(Point q, double r) : q(q), r(r) {}
 
-void RangeQuery::lookup(shared_ptr<Node> node, vector<Point> &answer) {
+int RangeQuery::lookup(shared_ptr<Node> node, vector<Point> &answer) {
     if (node->is_leaf()) {
         for (Entry entry : node->get_entries()) {
             if (dist(entry.get_p(), q) <= r) {
                 answer.push_back(Point(entry.get_p()));
             }
         }
+        return 1;
     } else {
+        int count = 0;
         for (Entry entry : node->get_entries()) {
             if (dist(entry.get_p(), q) <= r + entry.get_cr()) {
-                lookup(entry.get_a(), answer);
+                count += lookup(entry.get_a(), answer);
             }
         }
+        return count;
     }
-    return;
 }
